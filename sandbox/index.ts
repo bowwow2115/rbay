7c7e6b4b59926152964801ccab65dd1148a2f531
bowwow2115 <bowwow2115@gmail.com>
2026-02-06 18:10:16 +0900

    feat: 페이지 캐시 추가

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -1,5 +1,15 @@
 import 'dotenv/config';
 import { client } from '../src/services/redis';
 
-const run = async () => {};
+const run = async () => {
+	await client.del('car'); // Delete the old key first
+	await client.hSet('car', {
+		color: 'red',
+		year: 1950
+	});
+
+	const car = await client.hGetAll('car');
+
+	console.log(car);
+};
 run();
