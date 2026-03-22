e9ed7aed26f67ccc68fa2b624340323578f03bb6
bowwow2115 <bowwow2115@gmail.com>
2026-03-22 11:55:52 +0900

    feat: 아이템 조회수 기준 정렬 뷰 츄가

--- src/services/queries/items/by-views.ts
+++ src/services/queries/items/by-views.ts
@@ -1,1 +1,26 @@
-export const itemsByViews = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, count = 10) => {};
+import { client } from '$services/redis';
+import { itemsKey, itemsByViewsKey } from '$services/keys';
+import { deserialize } from './deserialize';
+export const itemsByViews = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, count = 10) => {
+	let result: any = await client.sort(itemsByViewsKey(), {
+		GET: [
+			'#',
+			`${itemsKey('*')}->name`,
+			`${itemsKey('*')}->views`,
+			`${itemsKey('*')}->endingAt`,
+			`${itemsKey('*')}->imageUrl`,
+			`${itemsKey('*')}->price`
+		],
+		BY: 'score',
+		DIRECTION: order,
+		LIMIT: { offset, count }
+	});
+	const items = [];
+	while (result.length) {
+		const [id, name, views, endingAt, imageUrl, price, ...rest] = result;
+		const item = deserialize(id, { name, views, endingAt, imageUrl, price });
+		items.push(item);
+		result = rest;
+	}
+	return items;
+};
